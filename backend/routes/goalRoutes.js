a0268e01ada1ae0ff4466b8bccd05c0ba4d384bc
Shaan Khela <Shanpreet.Singh1998@gmail.com>
2023-01-05 12:54:32 -0800

    Main push
    All files were added

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+
+const {
+    getGoal, 
+    setGoal, 
+    updateGoal, 
+    deleteGoal,} = require('../controllers/goalController')
+
+const {protect} = require('../middleware/authMiddleware')
+
+router.route('/').get(protect, getGoal).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
+
+module.exports = router
+
+
